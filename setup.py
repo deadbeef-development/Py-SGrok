46fcfe4aa246760c112c57b98fb5ba27716a714a
deadbeef-development <90785600+deadbeef-development@users.noreply.github.com>
2024-04-10 15:05:32 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Py-Wormgrok',
+    version='0.0.0',
+    packages=['wormgrok'],
+    author='deadbeef-development',
+    author_email='deadbeef.development@gmail.com'
+)
