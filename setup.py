a04d33cac6f3c43866ade1dfad7d3f126c5b6797
deadbeef-development <90785600+deadbeef-development@users.noreply.github.com>
2024-04-10 15:42:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='Py-Wormgrok',
     version='0.0.0',
     packages=['wormgrok'],
+    install_requires=[
+        'aiohttp~=3.9',
+        'ngrok~=1.2'
+    ],
     author='deadbeef-development',
     author_email='deadbeef.development@gmail.com'
 )
